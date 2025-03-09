9fe81c82c9d978f8e8b73af4377c5f497fccaadf
Daniel Asrat <Danielasratab@gmail.com>
2025-03-09 14:36:46 +0300

    adding files for production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const ibmPlexserif = IBM_Plex_Serif({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-ibm-plex-serif",
-});
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
 
 export const metadata: Metadata = {
-  title: "Amhara Bank",
-  description: "Amhara Bank is a Modern banking platform for everyone",
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
-    icon: "/icons/logo.svg",
-  },
+    icon: '/icons/logo.svg'
+  }
 };
 
 export default function RootLayout({
@@ -24,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}${ibmPlexserif.variable}`}>
-        {children}
-      </body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
     </html>
   );
 }
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,7 +1,8 @@
 import { formatAmount } from "@/lib/utils";
-import Link from "next/link";
-import React, { use } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
@@ -11,20 +12,21 @@ const BankCard = ({
   console.log(account);
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
-            <h1 className="text-16 font-semibold  text-white">
-              {account.name || userName}
-            </h1>
+            <h1 className="text-16 font-semibold text-white">{account.name}</h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
           </div>
-          <article className="flex flex-col gap-2 ">
-            {" "}
+
+          <article className="flex flex-col gap-2">
             <div className="flex justify-between">
-              <h1 className="text-12 font-semibold text-white ">{userName}</h1>
+              <h1 className="text-12 font-semibold text-white">{userName}</h1>
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
@@ -32,6 +34,7 @@ const BankCard = ({
             </p>
           </article>
         </div>
+
         <div className="bank-card_icon">
           <Image src="/icons/Paypass.svg" width={20} height={24} alt="pay" />
           <Image
@@ -42,6 +45,7 @@ const BankCard = ({
             className="ml-5"
           />
         </div>
+
         <Image
           src="/icons/lines.png"
           width={316}
@@ -50,7 +54,10 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.shareableId} />}
     </div>
   );
 };
+
 export default BankCard;
